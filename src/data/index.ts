73e3b734a66ba35d4c2969c7522b83c0e17a9ade
Matthew Williamson <matt.t.williamson@gmail.com>
2020-10-19 13:50:10 -0500

    Modeling the character sheet and health info

---
+++ src/data/index.ts
@@ -0,0 +1,60 @@
+export type CharacterId = number;
+
+type Name = string;
+type Level = number;
+
+type DiceType = 6 | 8 | 10 | 12;
+
+export type HitPoints = number;
+
+export interface CharacterClass {
+  name: Name;
+  hitDiceValue: DiceType;
+  classLevel: Level;
+}
+
+export type AbilityScore = number;
+
+export interface CharacterStats {
+  strength: AbilityScore;
+  dexterity: AbilityScore;
+  constitution: AbilityScore;
+  intelligence: AbilityScore;
+  wisdom: AbilityScore;
+  charisma: AbilityScore;
+}
+
+interface Modifier {
+  affectedObject: string;
+  affectedValue: string;
+  value: number;
+}
+
+export interface Item {
+  name: Name;
+  modifier: Modifier;
+}
+
+export type DamageType = 'fire' | 'slashing' | string;
+
+export type DefenseType = 'immunity' | 'resistance' | 'none';
+
+export interface Defense {
+  type: DamageType;
+  defense: DefenseType;
+}
+
+export interface Character {
+  name: Name;
+  level: Level;
+  classes: CharacterClass[];
+  stats: CharacterStats;
+  items: Item[];
+  defenses: Defense[];
+}
+
+export interface Health {
+  maximumHitPoints: HitPoints;
+  currentHitPoints: HitPoints;
+  temporaryHitPoints: HitPoints;
+}
