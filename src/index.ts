33a917da46be42a76c385a0d1375440348031cdc
Matthew Williamson <matt.t.williamson@gmail.com>
2020-10-19 13:29:13 -0500

    Adding scripts for building and running

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send({status: 'ok'})
+});
+
+app.listen(port, () => {
+  console.log(`server started on port: ${port}`)
+});
