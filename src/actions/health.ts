9a08691666803f1a95aa3aecac9a83f3c8d8b68a
Matthew Williamson <matt.t.williamson@gmail.com>
2020-10-20 01:27:10 -0500

    Adding action creators and a reducer for manipulating character health

---
+++ src/actions/health.ts
@@ -0,0 +1,71 @@
+import { calcDamage, calcHP } from "../core/health"
+import { Character, CharacterId, DamageType, Health, HitPoints } from "../data"
+
+type HealthActionTypes = 'HEALTH_SAVE' | 'HEALTH_HEAL' | 'HEALTH_DAMAGE' | 'HEALTH_TEMP_HP'
+
+export type HealthActions = SaveAction | HealAction | DamageAction | TempHPAction
+
+interface Action {
+  type: HealthActionTypes;
+}
+
+export interface SaveAction extends Action {
+  type: 'HEALTH_SAVE';
+  characterId: CharacterId;
+  health: Health;
+}
+
+export interface HealAction extends Action {
+  type: 'HEALTH_HEAL';
+  characterId: CharacterId;
+  value: number;
+}
+
+export interface DamageAction extends Action {
+  type: 'HEALTH_DAMAGE';
+  characterId: CharacterId;
+  actualDamage: number;
+}
+
+export interface TempHPAction extends Action {
+  type: 'HEALTH_TEMP_HP';
+  characterId: CharacterId;
+  temporaryHitPoints: HitPoints;
+}
+
+export function healthSaveAction(characterId: CharacterId, character: Character): SaveAction {
+  const maximumHitPoints = calcHP(character);
+  const health: Health = {
+    currentHitPoints: maximumHitPoints,
+    maximumHitPoints,
+    temporaryHitPoints: 0
+  }
+  return {
+    type: 'HEALTH_SAVE',
+    characterId,
+    health
+  }
+}
+
+export function healAction(characterId: CharacterId, value: number): HealAction {
+  return {
+    type: 'HEALTH_HEAL',
+    characterId,
+    value
+  }
+}
+
+export const damageAction = (characterId: CharacterId, character: Character, damageType: DamageType, damageValue: number): DamageAction => {
+  const actualDamage = calcDamage(character, damageType, damageValue);
+  return ({
+    type: 'HEALTH_DAMAGE',
+    characterId,
+    actualDamage
+  })
+}
+
+export const temporaryHPAction = (characterId: CharacterId, temporaryHitPoints: HitPoints): TempHPAction => ({
+  type: 'HEALTH_TEMP_HP',
+  characterId,
+  temporaryHitPoints
+})
---
+++ src/reducers/health.ts
@@ -0,0 +1,82 @@
+import { updateHealth, addTempHP } from "../core/health";
+import { CharacterId, Health } from "../data";
+import { DamageAction, HealAction, HealthActions, SaveAction, TempHPAction } from "../actions/health";
+
+interface HealthState {
+  health: Map<CharacterId, Health>;
+}
+
+const initialState: HealthState = {
+  health: new Map(),
+};
+
+// looks up the character id and heals them
+function healCharacter(prevState: HealthState, action: HealAction): HealthState {
+  const {characterId, value} = action;
+  const {health} = prevState;
+  const prevHealth = health.get(characterId)
+  if (prevHealth !== undefined) {
+    console.log(`healing characterId: ${characterId} +${value}`)
+    health.set(characterId, updateHealth(prevHealth, value));
+  }
+  return {
+    ...prevState,
+    health
+  };
+}
+
+// looks up the character id and damages their health
+function damageCharacter(prevState: HealthState, action: DamageAction): HealthState {
+  const {characterId, actualDamage} = action;
+  const {health} = prevState;
+  const prevHealth = health.get(characterId)
+  if (prevHealth !== undefined) {
+    console.log(`damaging characterId: ${characterId} ==> ${actualDamage}`)
+    health.set(characterId, updateHealth(prevHealth, -actualDamage));
+  }
+  return {
+    ...prevState,
+    health
+  };
+}
+
+// adds temporary hit points for a character id
+function tempHitPoints(prevState: HealthState, action: TempHPAction): HealthState {
+  const {characterId, temporaryHitPoints} = action;
+  const {health} = prevState;
+  const prevHealth = health.get(characterId);
+  if (prevHealth !== undefined) {
+    console.log(`adding temp HP for characterId: ${characterId} => ${temporaryHitPoints}`)
+    health.set(characterId, addTempHP(prevHealth, temporaryHitPoints));
+  }
+  return {
+    ...prevState,
+    health
+  };
+}
+
+// saves the initial health values for a character id
+function healthSave(prevState: HealthState, action: SaveAction) {
+  console.log(`saving health for characterId: ${action.characterId}`);
+  const {health} = prevState;
+  health.set(action.characterId, action.health);
+  return {
+    ...prevState,
+    health
+  };
+}
+
+export const reducer = (prevState = initialState, action: HealthActions): HealthState => {
+  switch (action.type) {
+    case 'HEALTH_SAVE': 
+      return healthSave(prevState, action)
+    case 'HEALTH_HEAL':
+      return healCharacter(prevState, action);
+    case 'HEALTH_DAMAGE':
+      return damageCharacter(prevState, action);
+    case 'HEALTH_TEMP_HP':
+      return tempHitPoints(prevState, action);
+    default:
+      return prevState;
+  }
+}
